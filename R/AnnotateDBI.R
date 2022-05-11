c8e32be1f4eb92230f8c2a115c68d521031b3f6c
Anisha Haldar <anishah@c0141.cm.cluster>
2022-05-11 08:44:58 -0500

    edits

--- R/AnnotateDBI.R
+++ R/AnnotateDBI.R
@@ -6,7 +6,6 @@ AnnotateDBI<-function(input_id,input_dataset,output_ids,input_species,output_spe
                      "Ensembl.id.version"="ENSEMBLIDVERSION",
                      "Gene.name"="GENENAME",
                      "Symbol"="SYMBOL")
-  return(output_ids)
   for(x in 1:length(output_ids)){
     output<-output_ids[x]
     output_ids[x] <- switch(output,
