19920b47735ff581615d786947d5641d092b8606
Anisha Haldar <anishahaldar@Anishas-MacBook-Pro.local>
2022-08-05 11:40:30 -0500

    edits

--- R/CoSIA-class.R
+++ R/CoSIA-class.R
@@ -78,8 +78,7 @@ CoSIAn <- function(gene_set, i_species, i_id, o_species, o_ids, mapping_tool="an
   map_species<- as.character(map_species)
   metric_type<- as.character(metric_type)
   new("CoSIAn", gene_set=gene_set, i_species=i_species, i_id=i_id, o_species=o_species, o_ids=o_ids, mapping_tool=mapping_tool, ortholog_database=ortholog_database, map_tissues=map_tissues, map_species=map_species, metric_type=metric_type)
-
-  }
+}
 
 ## Validity of the CoSIAn Class
 setValidity("CoSIAn", function(object) {
