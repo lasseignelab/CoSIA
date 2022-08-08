fa496c6d49bc81fedc3baae2fa6b7c3ed197a3d8
Anisha Haldar <anishahaldar@Anishas-MacBook-Pro.local>
2022-08-08 12:08:48 -0500

    formating updates to all scripts

--- R/getConversion-method.R
+++ R/getConversion-method.R
@@ -9,41 +9,47 @@
 #' getConversion(mouse_data)
 #' getConversion(rat_data)
 
-setMethod("getConversion", signature(object = "CosiaAnnotate"), function(object) { # user's input of the function
-  #set the parts of an object into variables
-  input_species<-object@input_species
-  input_id<-object@input_id
-  input<-object@input
-  output_ids<-object@output_ids
-  output_species<-object@output_species
-  tool<-object@tool
-  ortholog_database<-object@ortholog_database
-  if (input_species=="homo_sapiens"){ #code follows this path if the user chooses homo_sapiens as their input species
-    human_data<-homo_sapiens(input_id,input,output_ids,output_species, tool, ortholog_database)
-    #take this out if you are unable to fix it and make sure it works before you publish
-    return(human_data)
-  }
-  if (input_species=="d_melanogaster"){ #code follows this path if the user chooses d_melanogaster as their input species
-    fly_data<-d_melanogaster(input_id,input,output_ids,output_species, tool, ortholog_database)
-    return(fly_data)
-  }
-  if(input_species=="mus_musculus"){ #code follows this path if the user chooses mus_musculus as their input species
-    mouse_data<-mus_musculus(input_id,input,output_ids,output_species, tool, ortholog_database)
-    return(mouse_data)
-  }
-  if(input_species=="danio_rerio"){  #code follows this path if the user chooses danio_rerio as their input species
-    zebrafish_data<-danio_rerio(input_id,input,output_ids,output_species, tool, ortholog_database)
-    return(zebrafish_data)
-  }
-  if(input_species=="c_elegans"){  #code follows this path if the user chooses c_elegans as their input species
-    celegan_data<-c_elegans(input_id,input,output_ids,output_species, tool, ortholog_database)
-    return(celegan_data)
-  }
-  if(input_species=="r_norvegicus"){  #code follows this path if the user chooses r_norvegicus as their input species
-    rat_data<-r_norvegicus(input_id,input,output_ids,output_species, tool, ortholog_database)
-    return(rat_data)
-  }
-  else{ #code follows this path if the inout species is not correctly written or is not one of the available species.
-    stop("Error. Invalid input species. Make sure it matches the proper format")
-  }
+setMethod("getConversion", signature(object = "CosiaAnnotate"), function(object) {
+    # user's input of the function set the parts of an object into variables
+    input_species <- object@input_species
+    input_id <- object@input_id
+    input <- object@input
+    output_ids <- object@output_ids
+    output_species <- object@output_species
+    tool <- object@tool
+    ortholog_database <- object@ortholog_database
+    if (input_species == "homo_sapiens") {
+        # code follows this path if the user chooses homo_sapiens as their input species
+        human_data <- homo_sapiens(input_id, input, output_ids, output_species, tool, ortholog_database)
+        # take this out if you are unable to fix it and make sure it works before you publish
+        return(human_data)
+    }
+    if (input_species == "d_melanogaster") {
+        # code follows this path if the user chooses d_melanogaster as their input species
+        fly_data <- d_melanogaster(input_id, input, output_ids, output_species, tool, ortholog_database)
+        return(fly_data)
+    }
+    if (input_species == "mus_musculus") {
+        # code follows this path if the user chooses mus_musculus as their input species
+        mouse_data <- mus_musculus(input_id, input, output_ids, output_species, tool, ortholog_database)
+        return(mouse_data)
+    }
+    if (input_species == "danio_rerio") {
+        # code follows this path if the user chooses danio_rerio as their input species
+        zebrafish_data <- danio_rerio(input_id, input, output_ids, output_species, tool, ortholog_database)
+        return(zebrafish_data)
+    }
+    if (input_species == "c_elegans") {
+        # code follows this path if the user chooses c_elegans as their input species
+        celegan_data <- c_elegans(input_id, input, output_ids, output_species, tool, ortholog_database)
+        return(celegan_data)
+    }
+    if (input_species == "r_norvegicus") {
+        # code follows this path if the user chooses r_norvegicus as their input species
+        rat_data <- r_norvegicus(input_id, input, output_ids, output_species, tool, ortholog_database)
+        return(rat_data)
+    } else {
+        # code follows this path if the inout species is not correctly written or is not one of the available species.
+        stop("Error. Invalid input species. Make sure it matches the proper format")
+    }
 })
--- R/remove_version_number.R
+++ R/remove_version_number.R
@@ -1,64 +1,63 @@
-remove_version_number<- function(input_dataset, species){
-  if (species=="mus_musculus"){ # if the user's input species is a mouse the code follows this path
-    input_dataset<-data.frame(input_dataset) # create a new input dataset to manipulate
-    colnames(input_dataset)[1] <- "ENSEMBLIDVERSION" # set the column name as ensemblid
-    input_dataset = cbind(input_dataset, replicate(1,input_dataset$ENSEMBLIDVERSION)) # create a new identical column that will be used to maniputlate
-    ensembl_id<- input_dataset  # set the input dataset into a new column
-    ensembl_id<-data.frame(ensembl_id)
-    x<-1:(nrow(ensembl_id))
-    for (i in seq_along(x)) {
-      char<-input_dataset[i,1]
-      char1<-substr(char, start = 1, stop = 15)
-      input_dataset[i,1]<-char1
+remove_version_number <- function(input_dataset, species) {
+    if (species == "mus_musculus") {
+        # if the user's input species is a mouse the code follows this path
+        input_dataset <- data.frame(input_dataset)  # create a new input dataset to manipulate
+        colnames(input_dataset)[1] <- "ENSEMBLIDVERSION"  # set the column name as ensemblid
+        input_dataset = cbind(input_dataset, replicate(1, input_dataset$ENSEMBLIDVERSION))  # create a new identical column that will be used to maniputlate
+        ensembl_id <- input_dataset  # set the input dataset into a new column
+        ensembl_id <- data.frame(ensembl_id)
+        x <- 1:(nrow(ensembl_id))
+        for (i in seq_along(x)) {
+            char <- input_dataset[i, 1]
+            char1 <- substr(char, start = 1, stop = 15)
+            input_dataset[i, 1] <- char1
+        }
+        input_dataset <- data.frame(input_dataset)
+        colnames(input_dataset)[which(names(input_dataset) == "ENSEMBLIDVERSION")] <- "ENSEMBL"
+        colnames(input_dataset)[which(names(input_dataset) == "replicate.1..input_dataset.ENSEMBLIDVERSION.")] <- "ENSEMBLIDVERSION"
+        return(input_dataset)
     }
-    input_dataset<-data.frame(input_dataset)
-    colnames(input_dataset)[which(names(input_dataset) == "ENSEMBLIDVERSION")] <- "ENSEMBL"
-    colnames(input_dataset)[which(names(input_dataset) == "replicate.1..input_dataset.ENSEMBLIDVERSION.")] <- "ENSEMBLIDVERSION"
-    return (input_dataset)
-  }
-  
-  if (species == "homo_sapiens"){
-    input_dataset<-data.frame(input_dataset)
-    colnames(input_dataset)[1] <- "ENSEMBLIDVERSION"
-    input_dataset = cbind(input_dataset, replicate(1,input_dataset$ENSEMBLIDVERSION))
-    ensembl_id<- input_dataset
-    ensembl_id<-data.frame(ensembl_id)
-    x<-1:(nrow(ensembl_id))
-    for (i in seq_along(x)) {
-      char<-input_dataset[i,1]
-      char1<-substr(char, start = 1, stop = 18)
-      input_dataset[i,1]<-char1
+
+    if (species == "homo_sapiens") {
+        input_dataset <- data.frame(input_dataset)
+        colnames(input_dataset)[1] <- "ENSEMBLIDVERSION"
+        input_dataset = cbind(input_dataset, replicate(1, input_dataset$ENSEMBLIDVERSION))
+        ensembl_id <- input_dataset
+        ensembl_id <- data.frame(ensembl_id)
+        x <- 1:(nrow(ensembl_id))
+        for (i in seq_along(x)) {
+            char <- input_dataset[i, 1]
+            char1 <- substr(char, start = 1, stop = 18)
+            input_dataset[i, 1] <- char1
+        }
+        input_dataset <- data.frame(input_dataset)
+        colnames(input_dataset)[which(names(input_dataset) == "ENSEMBLIDVERSION")] <- "ENSEMBL"
+        colnames(input_dataset)[which(names(input_dataset) == "replicate.1..input_dataset.ENSEMBLIDVERSION.")] <- "ENSEMBLIDVERSION"
+        return(input_dataset)
     }
-    input_dataset<-data.frame(input_dataset)
-    colnames(input_dataset)[which(names(input_dataset) == "ENSEMBLIDVERSION")] <- "ENSEMBL"
-    colnames(input_dataset)[which(names(input_dataset) == "replicate.1..input_dataset.ENSEMBLIDVERSION.")] <- "ENSEMBLIDVERSION"
-    return (input_dataset)
-  }
-  
-  if (species=="danio_rerio"){
-    input_dataset<-data.frame(input_dataset)
-    colnames(input_dataset)[1] <- "ENSEMBLIDVERSION"
-    input_dataset = cbind(input_dataset, replicate(1,input_dataset$ENSEMBLIDVERSION))
-    ensembl_id<- input_dataset
-    ensembl_id<-data.frame(ensembl_id)
-    x<-1:(nrow(ensembl_id))
-    for (i in seq_along(x)) {
-      char<-input_dataset[i,1]
-      char1<-substr(char, start = 1, stop = 18)
-      input_dataset[i,1]<-char1
+
+    if (species == "danio_rerio") {
+        input_dataset <- data.frame(input_dataset)
+        colnames(input_dataset)[1] <- "ENSEMBLIDVERSION"
+        input_dataset = cbind(input_dataset, replicate(1, input_dataset$ENSEMBLIDVERSION))
+        ensembl_id <- input_dataset
+        ensembl_id <- data.frame(ensembl_id)
+        x <- 1:(nrow(ensembl_id))
+        for (i in seq_along(x)) {
+            char <- input_dataset[i, 1]
+            char1 <- substr(char, start = 1, stop = 18)
+            input_dataset[i, 1] <- char1
+        }
+        input_dataset <- data.frame(input_dataset)
+        colnames(input_dataset)[which(names(input_dataset) == "ENSEMBLIDVERSION")] <- "ENSEMBL"
+        colnames(input_dataset)[which(names(input_dataset) == "replicate.1..input_dataset.ENSEMBLIDVERSION.")] <- "ENSEMBLIDVERSION"
+        return(input_dataset)
     }
-    input_dataset<-data.frame(input_dataset)
-    colnames(input_dataset)[which(names(input_dataset) == "ENSEMBLIDVERSION")] <- "ENSEMBL"
-    colnames(input_dataset)[which(names(input_dataset) == "replicate.1..input_dataset.ENSEMBLIDVERSION.")] <- "ENSEMBLIDVERSION"
-    return (input_dataset)
-  }
-  
-  if (species == "c_elegans"){
-    stop("Error. Incorrect ID evaluation. C. elegans do not have Ensembl Ids with Version.")
-  }
-  
-  else{
-    stop("Error. Invalid species. Make sure it matches the proper format.")
-  }
-  
+
+    if (species == "c_elegans") {
+        stop("Error. Incorrect ID evaluation. C. elegans do not have Ensembl Ids with Version.")
+    } else {
+        stop("Error. Invalid species. Make sure it matches the proper format.")
+    }
+
 }
