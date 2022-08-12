bf742bf73b2cd74a9216af45efa99105dd805832
Anisha Haldar <anishahaldar@Anishas-MacBook-Pro.local>
2022-08-12 12:08:49 -0500

    edit

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,8 +1,23 @@
 setClass("CoSIA", representation("VIRTUAL"))  # virtual class
-setClass("CosiaAnnotate", contains = "CoSIA", slots = c(input = "character", input_species = "character", input_id = "character", output_ids = "character",
-    output_species = "character", tool = "character", ortholog_database = "character", data.frame = "data.frame"), prototype = list(input = character(0),
-    input_species = character(0), input_id = character(0), output_ids = character(0), output_species = character(0), tool = character(0), ortholog_database = character(0),
-    data.frame = data.frame(0)))
+setClass("CosiaAnnotate", contains = "CoSIA", 
+         slots = 
+           c(input = "character", 
+             input_species = "character", 
+             input_id = "character", 
+             output_ids = "character",
+             output_species = "character", 
+             tool = "character", 
+             ortholog_database = "character", 
+             data.frame = "data.frame"), 
+         prototype = 
+           list(input = character(0),
+                input_species = character(0), 
+                input_id = character(0), 
+                output_ids = character(0), 
+                output_species = character(0), 
+                tool = character(0), 
+                ortholog_database = character(0),
+                data.frame = data.frame(0)))
 setClass("CosiaExpressSpecies", contains = "CoSIA", slots = c(list_of_ensembl_ids = "character", list_of_respective_species = "character", single_tissue = "character",
     dataframe = "data.frame"), prototype = list(list_of_ensembl_ids = character(0), list_of_respective_species = character(0), single_tissue = character(0),
     dataframe = data.frame(0)))
