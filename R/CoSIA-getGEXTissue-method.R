01e0d9aa7047feaa5782d56ba9f804f524cf80ba
Anisha Haldar <anishahaldar@Anishas-MacBook-Pro.local>
2022-08-12 11:42:03 -0500

    edit

--- R/CoSIA-getGEXTissue-method.R
+++ R/CoSIA-getGEXTissue-method.R
@@ -1,26 +1,28 @@
-#CoSIAn getGexTissue
+#CoSIAn getGExTissue
 
-setMethod("getGexTissue", signature(object = "CoSIAn"), function(object) {
+setMethod("getGExTissue", signature(object = "CoSIAn"), function(object) {
   # user's input of the function
   if (object@map_tissues == "all_tissues") {
     gene_species <- object@map_species
-    #add an error here about gene species only being a length of 1
-    if (gene_species == "mus_musculus") {
-      bgee_species <- filter(Experimental_Hub_File, Species == "Mouse")
+    if(length(gene_species)> 1){
+      stop("Error: map_species slot has more than 1 species. This method only works if the map_species slot has only 1 species identified.")
     }
-    if (gene_species == "rattus_norvegicus") {
-      bgee_species <- filter(Experimental_Hub_File, Species == "Rat")
+    else{
+      if (gene_species == "m_musculus") {
+        bgee_species <- filter(Experimental_Hub_File, Species == "Mouse")
+      }
+      if (gene_species == "r_norvegicus") {
+        bgee_species <- filter(Experimental_Hub_File, Species == "Rat")
+      }
+      if (gene_species == "d_rerio") {
+        bgee_species <- filter(Experimental_Hub_File, Species == "Zebrafish")
+      }
+      if (gene_species == "h_sapiens") {
+        bgee_species <- filter(Experimental_Hub_File, Species == "Human")
+      }
     }
-    if (gene_species == "danio_rerio") {
-      bgee_species <- filter(Experimental_Hub_File, Species == "Zebrafish")
-    }
-    if (gene_species == "homo_sapiens") {
-      bgee_species <- filter(Experimental_Hub_File, Species == "Human")
-    }
-    species_specific <- data.frame(dplyr::select(bgee_species, Gene.ID, Experiment.ID, Anatomical.entity.ID, Anatomical.entity.name, Read.count,
-                                                 TPM, FPKM, Detection.flag))
-    species_specific$Anatomical.entity.name <- gsub("\"", "", species_specific$Anatomical.entity.name)
-    gene_specific_data <- dplyr::filter(species_specific, Gene.ID == object@single_gene)
+    #have to pull the conversion id here
+    gene_specific_data <- dplyr::filter(bgee_species, Gene.ID == object@converted_id)
     sample_size <- data.frame(table(gene_specific_data$Anatomical.entity.name))
     colnames(sample_size)[which(names(sample_size) == "Var1")] <- "Anatomical.entity.name"
     values <- aggregate(data = gene_specific_data, x = gene_specific_data$TPM, by = list(gene_specific_data$Anatomical.entity.name), FUN = median)
