120fcc16ad9dd5740aaf1d27923a70bb9b0a54d2
Anisha Haldar <anishahaldar@Anishas-MacBook-Pro.local>
2022-09-16 13:26:12 -0500

    updates to the ploting function

--- R/CoSIA-getPlot-method.R
+++ R/CoSIA-getPlot-method.R
@@ -7,7 +7,7 @@
 #'
 #' @examples
 
-setGeneric("plotSpeciesGEx", function(object) standardGeneric("plotSpeciesGEx"))
+setGeneric("plotSpeciesGEx", function(object, single_tissue, single_gene) standardGeneric("plotSpeciesGEx"))
 
 #' plotSpeciesGEx Method
 #'
@@ -18,24 +18,32 @@ setGeneric("plotSpeciesGEx", function(object) standardGeneric("plotSpeciesGEx"))
 #'
 #' @examples
 
-setMethod("plotSpeciesGEx", signature(object = "CoSIAn"), function(object) {
+setMethod("plotSpeciesGEx", signature(object = "CoSIAn"), function(object, single_tissue, single_gene) {
   gex_dataframe<-object@gex
+  converted_id<-object@converted_id
   gex_dataframe<- as.data.frame(gex_dataframe)
+  converted_id<- as.data.frame(converted_id)
   map_species<- object@map_species
-  map_tissues<- object@map_tissues
+  single_tissue<- single_tissue
+  list_of_ensembl_ids<- list_of_ensembl_ids
+  #filter gex_dataframe for single gene and single tissue
+  filtered_gex_dataframe<- dplyr::filter(gex_dataframe, Anatomical_entity_name == single_tissue & Ensembl_ID == list_of_ensembl_ids)
+  #color palette for plot (we can make this more modifiable)
   brewer.pal.info <- RColorBrewer::brewer.pal.info
   palette5 <- RColorBrewer::brewer.pal.info[brewer.pal.info$category == "qual", ]
   color <- unlist(mapply(RColorBrewer::brewer.pal, palette5$maxcolors, rownames(palette5)))
-
-fig <- plotly::plot_ly(gene_specific_data, x = ~AEN, y = ~TPM, type = "scatter", mode = "markers", color = ~AEN, colors = color) %>%
-  plotly::add_markers(x = ~AEN, y = ~x, marker = list(symbol = "line-ew", size = 10, line = list(color = "grey", width = 2)) %>%
+  #build plot
+  
+  fig <- plotly::plot_ly(filtered_gex_dataframe, x = ~Species, y = ~Median_TPM, type = "scatter", mode = "markers", color = ~Anatomical_entity_name, colors = color) %>%
+  
+  plotly::add_markers(x = ~Anatomical_entity_name, y = ~Species, marker = list(symbol = "line-ew", size = 10, line = list(color = "grey", width = 2)) %>%
                         plotly::add_trace(marker = list(size = 8, line = list(color = "black", width = 0.5)), showlegend = F))
 fig <- fig %>%
-  plotly::layout(title = stringr::str_wrap(paste("Gene Expression of the gene", object@single_gene, "in", object@gene_species, "amoung",
-                                                 object@tissues, sep = " ")), xaxis = list(title = "Anatomical Entity Name", size = 2), yaxis = list(title = "TPM (transcript per million)",
+  plotly::layout(title = stringr::str_wrap(paste("Gene Expression of the gene amoung",
+                                                 single_tissue, sep = " ")), xaxis = list(title = "Anatomical Entity Name", size = 2), yaxis = list(title = "TPM (transcript per million)",
                                                                                                                                                      zeroline = F), showlegend = FALSE)
 
-return(fig)
+  return(fig)
 
 
 brewer.pal.info <- RColorBrewer::brewer.pal.info
