cc427e00586c8d9b67f74c537b70679fb4fe47d8
Anisha Haldar <anishahaldar@Anishas-MacBook-Pro.local>
2022-10-03 14:54:41 -0500

    edits

--- R/CoSIA-getPlot-method.R
+++ R/CoSIA-getPlot-method.R
@@ -24,54 +24,50 @@ setMethod("plotSpeciesGEx", signature(object = "CoSIAn"), function(object, singl
   converted_id<-object@converted_id
   converted_id<- as.data.frame(converted_id)
   filter_ids<-dplyr::select(converted_id, ends_with("ensembl_id"))
-  filter_ids %>% tibble::as_tibble() %>% dplyr::filter_all(any_vars(. %in% single_gene))
-  
-  return(filter_ids)
-  
-  
-  map_species<- object@map_species
-  single_tissue<- single_tissue
-  single_gene<- single_gene
-  if (converted_id %in% single_gene ){
-    stop("Single gene is not in converted_id. Use getConversions to get the ensembl_id and ortholog_ensembl ids for species in the mapped_species slot")
-  }
-  converted_id<-converted_id %>% dplyr::filter(stringr::str_detect(single_gene))
-  return(converted_id)
-  
-  #filter gex_dataframe for single gene and single tissue
-  filtered_gex_dataframe<- dplyr::filter(gex_dataframe, Anatomical_entity_name == single_tissue & Ensembl_ID == list_of_ensembl_ids)
+  ids <- filter_ids[Reduce(`|`, lapply(filter_ids, grepl, pattern = single_gene)),]
+  ids<- as.character(ids)
+  filter_gex<-gex_dataframe[gex_dataframe$Ensembl_ID %in% ids, ]
+  filter_gex<-gex_dataframe[gex_dataframe$Anatomical_entity_name %in% single_tissue, ]
+  filter_gex<-separate_rows(filter_gex, TPM)
+  #add some validation methods here : check and make sure that the tissue is in gex the species in mapped species are in gex and that the single gene has been converted
   #color palette for plot (we can make this more modifiable)
   brewer.pal.info <- RColorBrewer::brewer.pal.info
   palette5 <- RColorBrewer::brewer.pal.info[brewer.pal.info$category == "qual", ]
   color <- unlist(mapply(RColorBrewer::brewer.pal, palette5$maxcolors, rownames(palette5)))
-  #build plot
+  
+  fig <- df %>%
+    plot_ly(
+      y = ~total_bill,
+      type = 'violin',
+      box = list(
+        visible = T
+      ),
+      meanline = list(
+        visible = T
+      ),
+      x0 = 'Total Bill'
+    ) 
+  
+  fig <- fig %>%
+    layout(
+      yaxis = list(
+        title = "",
+        zeroline = F
+      )
+    )
+  
+  fig
   
   
-  
-  fig <- plotly::plot_ly(filtered_gex_dataframe, x = ~Species, y = ~Median_TPM, type = "scatter", mode = "markers", color = ~Anatomical_entity_name, colors = color) %>%
-  
-  plotly::add_markers(x = ~Anatomical_entity_name, y = ~Species, marker = list(symbol = "line-ew", size = 10, line = list(color = "grey", width = 2)) %>%
-                        plotly::add_trace(marker = list(size = 8, line = list(color = "black", width = 0.5)), showlegend = F))
-fig <- fig %>%
-  plotly::layout(title = stringr::str_wrap(paste("Gene Expression of the gene amoung",
-                                                 single_tissue, sep = " ")), xaxis = list(title = "Anatomical Entity Name", size = 2), yaxis = list(title = "TPM (transcript per million)",
-                                                                                                                                                     zeroline = F), showlegend = FALSE)
-
+  fig <- plotly::plot_ly(filter_gex, x = ~Species, y = ~TPM, type = "scatter", mode = "markers", color = ~Species, colors = color)
+  fig <- fig %>%
+    plotly::add_trace(marker = list(size = 8, line = list(color = "black", width = 0.75)), showlegend = F)
+  fig <- fig %>%
+    plotly::layout(title = stringr::str_wrap(paste("Gene Expression of the gene", single_gene, "in", single_tissue, "across Species",
+                                                   sep = " ")), xaxis = list(title = "Anatomical Entity Name", size = 2), yaxis = list(title = "TPM (transcript per million)", zeroline = F),
+                   showlegend = FALSE)
   return(fig)
-
-
-brewer.pal.info <- RColorBrewer::brewer.pal.info
-palette5 <- RColorBrewer::brewer.pal.info[brewer.pal.info$category == "qual", ]
-color <- unlist(mapply(RColorBrewer::brewer.pal, palette5$maxcolors, rownames(palette5)))
-
-fig <- plotly::plot_ly(tissue_specific_data, x = ~AEN, y = ~TPM, type = "scatter", mode = "markers", color = ~AEN, colors = color) %>%
-  plotly::add_markers(x = ~AEN, y = ~x, marker = list(symbol = "line-ew", size = 10, line = list(color = "grey", width = 2)) %>%
-                        plotly::add_trace(marker = list(size = 8, line = list(color = "black", width = 0.5)), showlegend = F))
-fig <- fig %>%
-  plotly::layout(title = stringr::str_wrap(paste("Gene Expression of the gene", object@single_gene, "in", object@gene_species, "amoung",
-                                                 object@tissues, sep = " ")), xaxis = list(title = "Anatomical Entity Name", size = 2), yaxis = list(title = "TPM (transcript per million)",
-                                                                                                                                                     zeroline = F), showlegend = FALSE)
-
-return(fig)
-
 })
+  
+  
+    
