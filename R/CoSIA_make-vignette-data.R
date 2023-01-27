c75626cc97a6e4b5502c1faa77953cdb2fd534cf
Lasseigne Lab <lasseignelab@CDIB-Tabea-Soelter-MBP.local>
2023-01-27 14:42:45 -0500

    clean up R files

--- R/CoSIA_make-vignette-data.R
+++ R/CoSIA_make-vignette-data.R
@@ -1,19 +1,12 @@
-## CoSIA Vignette Data 
-## Amanda D. Clark
-## Created 01/10/23
-## Script is used to create data objects from the monogenic 
-## disease-associated kidney gene set from Natera. 
+## CoSIA Vignette Data Amanda D. Clark Created 01/10/23 Script is used to create data objects from the monogenic
+## disease-associated kidney gene set from Natera.
 
 
 # Load raw data
-monogenic_kidney_genes <- readr::read_csv(
-  file = "inst/extdata/raw/385_NateraKidney.csv", 
-  col_names = T)
+monogenic_kidney_genes <- readr::read_csv(file = "inst/extdata/raw/385_NateraKidney.csv", col_names = T)
 
 # Output RDS
-saveRDS(object = monogenic_kidney_genes, 
-        file = "inst/extdata/proccessed/monogenic_kidney_genes.rds")
+saveRDS(object = monogenic_kidney_genes, file = "inst/extdata/proccessed/monogenic_kidney_genes.rds")
 
 # Output RDA
-save(monogenic_kidney_genes, 
-     file = "inst/extdata/proccessed/monogenic_kidney_genes.rda")
+save(monogenic_kidney_genes, file = "inst/extdata/proccessed/monogenic_kidney_genes.rda")
