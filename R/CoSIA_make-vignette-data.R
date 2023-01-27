0b6342e04ba17d0098fa0f14ebb4815cde830776
Lasseigne Lab <lasseignelab@CDIB-Tabea-Soelter-MBP.local>
2023-01-27 14:17:18 -0500

    removed library (error in bioconductor check)

--- R/CoSIA_make-vignette-data.R
+++ R/CoSIA_make-vignette-data.R
@@ -5,11 +5,8 @@
 ## disease-associated kidney gene set from Natera. 
 
 
-# Set up
-library(readr)
-
 # Load raw data
-monogenic_kidney_genes <- read_csv(file = "inst/extdata/raw/385_NateraKidney.csv", col_names = T)
+monogenic_kidney_genes <- readr::read_csv(file = "inst/extdata/raw/385_NateraKidney.csv", col_names = T)
 
 # Output RDS
 saveRDS(object = monogenic_kidney_genes, file = "inst/extdata/proccessed/monogenic_kidney_genes.rds")
